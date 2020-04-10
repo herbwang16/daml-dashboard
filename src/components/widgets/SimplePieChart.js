0cc24cc6ba55e0592459852cf0fefa21dcfc56c9
NICOLE MOISEYEV <nicole.moiseyev3400@gmail.com>
2020-04-09 22:42:43 -0400

    Make pie chart component scalable

--- src/components/widgets/SimplePieChart.js
+++ src/components/widgets/SimplePieChart.js
@@ -41,14 +41,11 @@ class SimplePieChart extends React.Component {
   render() {
     return (
       <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
+        <PieChart>
           <Pie
             data={data}
-            cx={200}
-            cy={200}
             labelLine={false}
             label={renderCustomizedLabel}
-            outerRadius={80}
             fill="#8884d8"
             dataKey="value"
           >
