da3a1dac52617bd58a495f24799a7b333990bd2b
Rachel Ma <rachel.ma@duke.edu>
2020-06-22 20:16:36 -0400

    added router to table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./components/layout/Homepage";
@@ -11,9 +13,31 @@ import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 function App() {
   return (
-    <ThemeProvider>
-      <Homepage />
-    </ThemeProvider>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <ThemeProvider>
+              <Homepage />
+            </ThemeProvider>
+            <li>
+              <Link to="/table">Table</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Homepage />
+          </Route>
+          <Route path="/table">
+            <Table />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
