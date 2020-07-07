6fa1542cc0946971d7a2aeaac0ed2932fb7f7107
Alyssa Z <60488436+vAl9719@users.noreply.github.com>
2020-07-06 21:18:02 -0400

    Update SideBar.js

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "../../index.css";
+import "../../App.css";
 
 import { Layout, Menu} from 'antd';
-import { UserOutlined, ProfileFilled, BlockOutlined, SettingFilled} from '@ant-design/icons';
+import { UserOutlined, ProfileFilled, BlockOutlined, SettingFilled, FileAddFilled, SwitcherOutlined} from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 const { Sider } = Layout;
@@ -24,7 +24,7 @@ class NavBar extends React.Component {
 
       <Layout>
       <Sider width={317} className="site-layout-background">
-      <div className="logo"><div className="daml"><UserOutlined />DAML</div></div>
+      <div className="logo"><div className="daml"><span><UserOutlined />DAML</span></div></div>
         <Menu
           mode="inline"
           /* defaultSelectedKeys={['1']}
@@ -39,9 +39,11 @@ class NavBar extends React.Component {
             <span>My Dashboards</span>
             </span>}>
             <Menu.Item key="1" className="menu-item"><Option opt="BSR v1"/></Menu.Item>
-            <Menu.Item key="2" className="menu-item"><Option opt="Phoenix Project" /></Menu.Item>
+            <Menu.Item key="2" className="menu-item" ><Option opt="Phoenix Project" /></Menu.Item>
             <Menu.Item key="3" className="menu-item"><Option opt="DoD" /></Menu.Item>
+            <Menu.Item key="4" className="menu-item"><FileAddFilled />Add Dashboard</Menu.Item>
           </SubMenu>
+
 
           <SubMenu key="sub2" className="main-menu" title={
               <span>
@@ -51,32 +53,36 @@ class NavBar extends React.Component {
           }
           >
             <SubMenu key="g1" className="option" title={<Option opt="BSR v1" />}>
-            <Menu.Item key="4" className="submenu-data"><Option opt="PeanutButter.csv"/></Menu.Item>
-            <Menu.Item key="5" className="submenu-data"><Option opt="BSR API"/></Menu.Item>
-            <Menu.Item key="6" className="submenu-data"><Option opt="Sale.csv"/></Menu.Item>
+            <Menu.Item key="5" className="submenu-data">
+              <Option opt={<span><SwitcherOutlined/>PeanutButter.csv</span>}/>
+            </Menu.Item>
+            <Menu.Item key="6" className="submenu-data"><Option opt={<span><SwitcherOutlined/>BSR API</span>}/></Menu.Item>
+            <Menu.Item key="7" className="submenu-data"><Option opt={<span><SwitcherOutlined/>Sales.csv</span>}/></Menu.Item>
             </SubMenu>
 
             <SubMenu key="g2" className="option" title={<Option opt="Phoenix Project" />}>
-            <Menu.Item key="7" className="submenu-data" ><Option opt="Companies.csv"/></Menu.Item>
-            <Menu.Item key="8" className="submenu-data"><Option opt="Participants.csv"/></Menu.Item>
-            <Menu.Item key="9" className="submenu-data"><Option opt="hey there"/></Menu.Item>
+            <Menu.Item key="8" className="submenu-data" ><Option opt={<span><SwitcherOutlined/>Companies.csv</span>}/></Menu.Item>
+            <Menu.Item key="9" className="submenu-data"><Option opt={<span><SwitcherOutlined/>Participants.csv</span>}/></Menu.Item>
+            <Menu.Item key="10" className="submenu-data"><Option opt={<span><SwitcherOutlined/>PeanutButter.csv</span>}/></Menu.Item>
             </SubMenu>
 
             <SubMenu key="g3" className="option" title={<Option opt="DoD" />}>
-            <Menu.Item key="10" className="submenu-data"><Option opt="Jelly.cvs"/></Menu.Item>
-            <Menu.Item key="11" className="submenu-data"><Option opt="BeeMovieScript.txt"/></Menu.Item>
-            <Menu.Item key="12" className="submenu-data"><Option opt="what?"/></Menu.Item>
+            <Menu.Item key="11" className="submenu-data" ><Option opt={<span><SwitcherOutlined/>Jelly.csv</span>}/></Menu.Item>
+            <Menu.Item key="12" className="submenu-data"><Option opt={<span><SwitcherOutlined/>BeeMovieScript.txt</span>}/></Menu.Item>
+            <Menu.Item key="13" className="submenu-data"><Option opt={<span><SwitcherOutlined/>image.png</span>}/></Menu.Item>
             </SubMenu>
           </SubMenu>
           <SubMenu key="sub3" className="main-menu" title={
               <span>
             <SettingFilled/>
+
+            
             <span>Account Settings</span>
             </span>
           }>
-            <Menu.Item key="13" className="submenu-data">Payment</Menu.Item>
-            <Menu.Item key="14" className="submenu-data">Profile</Menu.Item>
-            <Menu.Item key="15" className="submenu-data">History</Menu.Item>
+            <Menu.Item key="14" className="submenu-data">Payment</Menu.Item>
+            <Menu.Item key="15" className="submenu-data">Profile</Menu.Item>
+            <Menu.Item key="16" className="submenu-data">History</Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
@@ -86,4 +92,3 @@ class NavBar extends React.Component {
 }
 
 export default NavBar;
-
