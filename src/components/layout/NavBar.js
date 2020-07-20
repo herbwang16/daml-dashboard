73914a09a7ae48e2909c41551010d7a09f127e72
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2020-07-20 18:00:42 -0400

    logout

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,11 +2,20 @@ import React from "react";
 import "../../App.css";
 import {Layout, Button} from "antd";
 import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { withRouter } from 'react-router-dom';
 import {Logout as logout} from "../../api/api";
 const { Header } = Layout;
 
 class NavBar extends React.Component{
    
+  handleClick = async () => {
+    await logout(localStorage.getItem('token'))
+      .catch(err => {
+        console.log(err)
+      });
+    localStorage.clear();
+    this.props.history.push('/login');
+  }
    
    render(){
      return (
@@ -14,7 +23,7 @@ class NavBar extends React.Component{
     <Header className="header">
       <div className="title">{this.props.title}</div>
       <div className="user"><span>Welcome, {this.props.user} <UserOutlined/></span></div>
-    <div className="logout-button"><Button onClick={console.log("click")} style={{height:"40px"}}>
+    <div className="logout-button"><Button onClick={this.handleClick} style={{height:"40px"}}>
       <span><LogoutOutlined/>   Logout</span>
     </Button></div>
     </Header>
@@ -24,4 +33,4 @@ class NavBar extends React.Component{
    
 };
 
-export default NavBar;
+export default withRouter(NavBar);
