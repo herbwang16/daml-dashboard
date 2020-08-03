3cf71c852e97e3243f544ecb92a4c9390c938f70
Nathaniel Friedman <nathaniel.friedman@duke.edu>
2020-08-03 19:48:02 -0400

    delete data changes and debug

--- src/components/layout/Homepage.js
+++ src/components/layout/Homepage.js
@@ -50,7 +50,7 @@ class Homepage extends React.Component {
         );
       default:
         return (
-          <div style = {{lineHeight: 1.2,  position: 'absolute', fontSize: 100, left: 525, top: 300}}>
+          <div style = {{lineHeight: 1.2,  position: 'absolute', fontSize: 100, left: 375, top: 175}}>
             Welcome, create or choose a dashboard
           </div>
         )
